6361840ee315435a9b3e1c3cc5ee96a800a83a6b
David Quiñones <daquinons@users.noreply.github.com>
2019-05-17 10:44:46 +0200

    Add infinite loop carousel

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -1,8 +1,38 @@
 class Carousel {
+    constructor(domElement) {
+        this.element = domElement;
+        this.leftButton = domElement.querySelector('.left-button');
+        this.leftButton.addEventListener('click', () => this.changeImage('previous'));
+        this.rightButton = domElement.querySelector('.right-button');
+        this.rightButton.addEventListener('click', () => this.changeImage('next'));
+        this.images = domElement.querySelectorAll('img');
+        this.currentIndex = 0;
+        this.displayImage();
+    }
+
+    displayImage() {
+        this.currentIndex = this.currentIndex < 0 ? this.images.length - 1 : this.currentIndex % this.images.length;
+        let image = this.images[this.currentIndex];
+        image.style.display = 'block';
+    }
+
+    changeImage(action) {
+        let currentImage = this.images[this.currentIndex];
+        currentImage.style.display = 'none';
+        
+        if (action === 'next') {
+            this.currentIndex += 1;
+        } else if (action === 'previous') {
+            this.currentIndex -= 1;
+        }
+
+        this.displayImage();
+    }
 
 }
 
-let carousel = document.querySelector();
+let carousel = document.querySelector('.carousel');
+new Carousel(carousel);
 
 /* If You've gotten this far, you're on your own! Although we will give you some hints:
     1. You will need to grab a reference to the carousel, and in it grab the left and right buttons
